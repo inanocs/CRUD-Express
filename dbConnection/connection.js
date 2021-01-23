89c0a9a7d017f0502df7107f72bc510dbd170735
inanocs <nanocabezasanchez@gmail.com>
2021-01-23 14:00:22 +0100

    Socios DONE

--- dbConnection/connection.js
+++ dbConnection/connection.js
@@ -1,7 +1,7 @@
 const mysql = require("mysql2");
 
 const conn = mysql.createConnection({
-  host: "192.168.1.3",
+  host: "localhost",
   user: "biblioteca",
   password: "biblioteca",
   database: "biblioteca",
